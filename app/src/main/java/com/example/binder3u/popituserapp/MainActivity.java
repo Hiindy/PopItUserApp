3713dc420c5ff49ad53f3e2178167f1630e2ad85
Cindy <BinderCindy@hotmail.fr>
2018-03-14 16:24:35 +0100

    Début interfaces

--- app/src/main/java/com/example/binder3u/popituserapp/MainActivity.java
+++ app/src/main/java/com/example/binder3u/popituserapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.binder3u.popituserapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialisation();
     }
+
+    public void initialisation() {
+        // Get reference of widgets from XML layout
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_prenoms);
+
+        // Initializing a String Array
+        String[] names = new String[]{
+                "Camille",
+                "Micheline",
+                "Claire",
+                "Quentin",
+                "Mouloud"
+        };
+
+        // Initializing an ArrayAdapter
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this,R.layout.spinner_item,names
+        );
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter(spinnerArrayAdapter);
+    }
+
+
 }
